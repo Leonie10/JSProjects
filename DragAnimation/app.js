9ce0edc8bcab2aefcb660de4a5aa38d91c9d1b77
Leonie10 <leonie.10@hotmail.fr>
2020-11-12 09:12:05 +0100

    finish DragAnimation

--- DragAnimation/app.js
+++ DragAnimation/app.js
@@ -2,6 +2,8 @@ let drag = document.querySelector('.drag');
 let drop1 = document.querySelector('.drop1');
 let drop2 = document.querySelector('.drop2');
 let future = document.querySelector('.future');
+let translate, translateX;
+let initialFuturePos = window.innerWidth + (0.1 * window.innerWidth);
 
 
 drag.addEventListener('dragstart', dragStart);
@@ -17,10 +19,9 @@ function dragStart (){
 
 
 function dragEl (e){
-    let initialFuturePos = window.innerWidth + (0.1 * window.innerWidth)  ;
-    future.style.transform = 'translateX(-' + -(e.clientX - initialFuturePos) +'px) ';
+    stopTranslate(e);
+    future.style.transform = 'translateX('+ translateX +'px)' ;
 }
-
 
 
 function dragEnd() {
@@ -28,13 +29,24 @@ function dragEnd() {
 }
 
 
-// Drop1
+
+function stopTranslate (e) {
+    translate = e.clientX - initialFuturePos;
+    if(-translate < window.innerWidth){
+        translateX = translate;
+    } else if( -(translate + 3) === window.innerWidth ){
+        translateX = 0;
+    } 
+    return translateX;
+}
+
+
 drop1.addEventListener('dragover', dragOver, false);
 drop1.addEventListener('dragenter', dragEnter, false);
 drop1.addEventListener('dragleave', dragLeave, false);
 drop1.addEventListener('drop', dropDrag, false);
 
-// Drop2
+
 drop2.addEventListener('dragover', dragOver, false);
 drop2.addEventListener('dragenter', dragEnter, false);
 drop2.addEventListener('dragleave', dragLeave, false);
@@ -50,7 +62,13 @@ function dragEnter (event) {
 function dragLeave () {
     
 }
-function dropDrag () {
+function dropDrag (e) {
+    if ( this.classList[1] === 'drop2'){
+        future.style.transform = 'translateX(0px)';
+        future.style.left = '100%';
+    } else if(e.clientX < (window.innerWidth / 2)){
+        future.style.transform = 'translateX(' + -window.innerWidth + 'px)';
+    }
     this.append(drag);
-    
 }
+
